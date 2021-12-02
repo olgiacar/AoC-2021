5f1bd7b0e72ff899b1cad1bc6f05ac3a4e7dda03
Carlos Olgiati <olgiacar@students.zhaw.ch>
2021-12-02 08:54:07 +0100

    Solution day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,9 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
+    val lines = readInput("input-01").map { it.toInt() }
+    val totalPartOne = lines.zipWithNext { a, b -> if (b > a) 1 else 0 }.sum()
+    println(totalPartOne)
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
-
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+    val totalPartTwo = lines.windowed(3).map { it.sum() }
+        .zipWithNext { a, b -> if (b > a) 1 else 0 }.sum()
+    println(totalPartTwo)
 }
